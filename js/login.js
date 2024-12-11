078e22a9de30a3d2815c649cf01904a4b24c24fc
Alex <alexisbenitez173@gmail.com>
2024-12-11 10:52:54 -0300

    login finalizado

---
+++ js/login.js
@@ -0,0 +1,66 @@
+/*
+1.TOMAR EL FORMULARIO
+
+2.TOMAR LOS DATOS CARGADOS
+
+3. VER SI EXISTE UN EMAIL COMO EL QUE EL USUARIO INGRESA
+
+4.CORROBAR QUE EXISTA LA CONTRASEÑA
+
+
+
+HACER EL LOGIN
+
+*/
+
+
+const users = JSON.parse (localStorage.getItem("users")) || []
+
+const loginForm = document.getElementById("login-form") 
+
+loginForm.addEventListener("submit" , (evt) => {
+    evt.preventDefault
+
+    const email = loginForm.nextElementSibling.email.value
+    const password = evt.target.elements.password.value
+
+    const user = users.find ((usr) => {
+
+        if(usr.email.toLowerCase === email.toLowerCase ()) {
+            return true
+        }
+
+        return false
+    })
+
+    if(!user || user.password !== password) {
+        Swal.fire({
+            icon: 'error' , 
+            title: 'Login Incorrecto',
+            text: 'Alguno de los datos no es correcto',
+            timer: 2000
+        })
+
+        return
+    }
+
+    delete user.password
+    localStorage.setItem("currentUser", JSON.stringify (user))
+
+    Swal.fire ({
+        icon: 'success' ,
+        title: 'Login Correcto',
+        text: 'Será redireccionado en un momento'
+        
+    })
+
+    console.log ('Iniciando timeout')
+
+    setTimeout(function() {
+
+        window.location.href = '/index.html'
+    },
+    2500)
+
+})
+
